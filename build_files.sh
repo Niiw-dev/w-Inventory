4d479a1b691b944e4b4f4089160281538563ce26
edwin.correa <edwin.correa@savne.net>
2026-03-12 15:21:17 -0500

    modificación de build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Salir inmediatamente si ocurre un error
+set -e
+
+echo "Instalando dependencias..."
+python3 -m pip install -r requirements.txt
+
+echo "Recolectando archivos estáticos..."
+python3 manage.py collectstatic --noinput --clear
+
+echo "Build finalizado con éxito."

cc92820155e6d40918f385401826b31a1f7541d8
IInw <niiw.dev@gmail.com>
2026-03-15 19:39:51 -0500

    Intengo de subir ajustes de colores

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "BUILD START"
 
-# Crear y activar entorno virtual
 python3.9 -m venv venv
 source venv/bin/activate
 
-# Instalar dependencias dentro del venv
 pip install -r requirements.txt
 
-# Ejecutar collectstatic
+# instalar node si existe package.json
+if [ -f package.json ]; then
+    npm install
+    npx gulp
+fi
+
 python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
